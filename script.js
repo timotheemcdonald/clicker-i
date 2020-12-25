0dbebb1be7f0f46c6ef432a4080db317e6bd1e19
timotheemcdonald <timotheemcdonald@gmail.com>
2020-12-25 11:19:26 -0500

    added functions to buy cursors and grammas and autoincrement for score

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 let score = 0;
+document.getElementById("score").innerHTML = score;
 
+let cursorCost = 15;
+let cursors = 0;
+document.getElementById("cursorCost").innerHTML = cursorCost;
+document.getElementById("cursors").innerHTML = cursors;
+
+let grammaCost = 100;
+let grammas = 0;
+document.getElementById("grammaCost").innerHTML = grammaCost;
+document.getElementById("grammas").innerHTML = grammas;
+
+function buyCursor(){
+    if(score >= cursorCost){
+        score = score - cursorCost;
+        cursors = cursors +1;
+        cursorCost = Math.round(cursorCost * 1.15);
+
+        document.getElementById("score").innerHTML = score;
+        document.getElementById("cursorCost").innerHTML = cursorCost;
+        document.getElementById("cursors").innerHTML = cursors;
+    }
+}
+
+function buyGramma(){
+    if(score >= grammaCost){
+        score = score - grammaCost;
+        grammas = grammas + 1;
+        grammaCost = Math.round(grammaCost * 1.15);
+
+        document.getElementById("score").innerHTML = score;
+        document.getElementById("grammaCost").innerHTML = grammaCost;
+        document.getElementById("grammas").innerHTML = grammas;
+    }
+
+}
 
 function increaseScore(amount){
 
@@ -7,3 +42,10 @@ function increaseScore(amount){
     document.getElementById("score").innerHTML = score;  
       
 }
+
+setInterval( () => {
+    score = score + cursors;
+    score = score + grammas * 5;
+    document.getElementById("score").innerHTML = score;  
+}, 1000) 
+//1000 = 1000ms = 1 second
